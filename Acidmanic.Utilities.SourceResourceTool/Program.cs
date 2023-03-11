d413c69cbf979b1f296abeddfc8b20f80e86c408
Mani Moayedi <acidmanic.moayedi.com>
2023-03-11 20:37:54 +0330

    Add epty project for nuget tool

---
+++ Acidmanic.Utilities.SourceResourceTool/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Acidmanic.Utilities.SourceResourceTool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
